c2897e2c48bfa7e2e0cb5a644d6fa4c4bf816605
Simeon van der Steen <simeon.vandersteen@bbc.co.uk>
2016-01-13 11:40:02 +0000

    add some sleep to wait for db to go up

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,6 +4,8 @@
 set -e
 
 if [ "$1" = 'spindle' ]; then
+	echo "Sleeping for 5 seconds to make sure database is initialised.."
+	sleep 5
 	/usr/bin/twine -S || exit $?
 	exec /usr/sbin/apache2 -DFOREGROUND
 fi
