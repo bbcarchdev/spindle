674c560cbb922eccdcf76d6530cd5fb765f79277
Simeon van der Steen <simeon.vandersteen@bbc.co.uk>
2016-05-27 14:02:29 +0100

    Collection test should include extra pages of results, and also count sonnets and poems

--- docker/cucumber/features/step_definitions/ingest.rb
+++ docker/cucumber/features/step_definitions/ingest.rb
@@ -2,6 +2,8 @@
 When(/^"([^"]*)" is ingested into Twine$/) do |file|
     # POST the to the remote control the file to be ingested
     http = Net::HTTP.new('twine', 8000)
+    http.read_timeout = 300
+
     request = Net::HTTP::Post.new("/ingest")
     request.add_field('Content-Type', 'text/x-nquads')
     request.body = IO.read(file)
@@ -16,7 +18,8 @@ When(/^"([^"]*)" is ingested into Twine$/) do |file|
 end
 
 When(/^I count the amount of relevant entities that are ingested$/) do
-        @entities = count("http://quilt/everything.nt?limit=200")
+        @entities = count("http://quilt/everything.nt")
+        puts "(#{@entities} entities found)"
 end
 
 When(/^A collection exists for "([^"]*)"$/) do |c_uri|
@@ -26,11 +29,11 @@ When(/^A collection exists for "([^"]*)"$/) do |c_uri|
         response = Net::HTTP.get_response(uri)
 
         expect(response).to be_a(Net::HTTPSeeOther)
-        @collection = response['location']
+        @collection = response['location'].chomp('#id')
 end
 
 Then(/^The number of relevant entities in the collection should be the same$/) do
-        n = count("http://quilt#{@collection}.nt?limit=200")
+        n = count("http://quilt#{@collection}.nt")
 
         expect(n).to eq(@entities)
 end
