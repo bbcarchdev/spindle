04341725dc4e2dce84f1b4cb6937bcb6caa7658b
Simeon van der Steen <simeon.vandersteen@bbc.co.uk>
2016-07-05 15:41:02 +0100

    Revert "Avoid loading all the incoming links to a license when updating that license"
    This reverts commit 4b8572189ee05857c627ca25f8fc08def56d0373.

--- twine/generate/source.c
+++ twine/generate/source.c
@@ -167,54 +167,28 @@ spindle_source_fetch_db_(SPINDLEENTRY *data)
 	size_t c;
 
 	r = 0;
-
-	if(data->flags & TK_MEDIA)
+	for(c = 0; data->refs[c]; c++)
 	{
-		for(c = 0; data->refs[c]; c++)
+		/* Add <ref> owl:sameAs <localname> triples to the proxy model */
+		if(sparql_queryf_model(data->sparql, data->sourcedata,
+			"SELECT DISTINCT ?s ?p ?o ?g\n"
+			" WHERE {\n"
+			"  GRAPH ?g {\n"
+			"  { <%s> ?p ?o .\n"
+			"   BIND(<%s> as ?s)\n"
+			"  }\n"
+			"  UNION\n"
+			"  { ?s ?p <%s> .\n"
+			"   BIND(<%s> as ?o)\n"
+			"  }\n"
+			" }\n"
+			"}",
+			data->refs[c], data->refs[c], data->refs[c], data->refs[c]))
 		{
-			/* Add <ref> owl:sameAs <localname> triples to the proxy model */
-			if(sparql_queryf_model(data->sparql, data->sourcedata,
-				"SELECT DISTINCT ?s ?p ?o ?g\n"
-				" WHERE {\n"
-				"  GRAPH ?g {\n"
-				"  { <%s> ?p ?o .\n"
-				"   BIND(<%s> as ?s)\n"
-				"  }\n"
-				" }\n"
-				"}",
-				data->refs[c], data->refs[c]))
-			{
-				r = -1;
-				break;
-			}
+			r = -1;
+			break;
 		}
 	}
-	else
-	{
-		for(c = 0; data->refs[c]; c++)
-		{
-			/* Add <ref> owl:sameAs <localname> triples to the proxy model */
-			if(sparql_queryf_model(data->sparql, data->sourcedata,
-				"SELECT DISTINCT ?s ?p ?o ?g\n"
-				" WHERE {\n"
-				"  GRAPH ?g {\n"
-				"  { <%s> ?p ?o .\n"
-				"   BIND(<%s> as ?s)\n"
-				"  }\n"
-				"  UNION\n"
-				"  { ?s ?p <%s> .\n"
-				"   BIND(<%s> as ?o)\n"
-				"  }\n"
-				" }\n"
-				"}",
-				data->refs[c], data->refs[c], data->refs[c], data->refs[c]))
-			{
-				r = -1;
-				break;
-			}
-		}
-	}
-
 	return r;
 }
 
