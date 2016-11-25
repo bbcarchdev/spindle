bbcarchdev/spindle

commit 7f845ba6ca1fada3a034f6473c0c86c140228f62

Ensure we don't create a loop when adding a membership relation and only add a trigger TK_MEMBERSHIP if such a relation was added (hence new and not creating loops). Fixes #94

twine/generate/index-membership.c
@@ -243,9 +243,19 @@ spindle_index_membership_add_uri_(SPINDLEENTRY *data, SQL *sql, const char *id, 
 			spindle_trigger_add(data, uristr, TK_MEMBERSHIP, NULL);
 			return 0;
 		}
-		spindle_trigger_add(data, uristr, TK_MEMBERSHIP, collid);
+
+		// Add the membership relation
 		r = spindle_index_membership_add_(sql, id, collid);
+
+		// If the link was freshly added (r=0) add a matching trigger
+		// r=1 if the link was already there or would have created a loop
+		// r=-1 in case of error trying to add the link
+		if (!r)
+		{
+			spindle_trigger_add(data, uristr, TK_MEMBERSHIP, collid);
+		}
 		free(collid);
+
 		return r;
 	}
 	localuri = spindle_proxy_locate(data->spindle, uristr);
@@ -275,6 +285,7 @@ spindle_index_membership_add_(SQL *sql, const char *id, const char *collid)
 {
 	SQL_STATEMENT *rs;
 
+	// Check if the relation is already there
 	rs = sql_queryf(sql, "SELECT \"id\" FROM \"membership\" WHERE \"id\" = %Q AND \"collection\" = %Q", id, collid);
 	if(!rs)
 	{
@@ -283,13 +294,31 @@ spindle_index_membership_add_(SQL *sql, const char *id, const char *collid)
 	if(!sql_stmt_eof(rs))
 	{
 		sql_stmt_destroy(rs);
-		return 0;
+		return 1;
 	}
 	sql_stmt_destroy(rs);
+
+	// Ensure we won't create a loop by adding it. If the loop was to be
+	// created don't add the link and return a 1
+	rs = sql_queryf(sql, "SELECT \"id\" FROM \"membership\" WHERE \"id\" = %Q AND \"collection\" = %Q", collid, id);
+	if(!rs)
+	{
+		return -1;
+	}
+	if(!sql_stmt_eof(rs))
+	{
+		sql_stmt_destroy(rs);
+		return 1;
+	}
+	sql_stmt_destroy(rs);
+
+	// Add the direct relation
 	if(sql_executef(sql, "INSERT INTO \"membership\" (\"id\", \"collection\") VALUES (%Q, %Q)", id, collid))
 	{
 		return -1;
 	}
+
+	// Recursively add all the membership
 	rs = sql_queryf(sql, "SELECT \"collection\" FROM \"membership\" WHERE \"id\" = %Q", collid);
 	if(!rs)
 	{
@@ -300,6 +329,7 @@ spindle_index_membership_add_(SQL *sql, const char *id, const char *collid)
 		spindle_index_membership_add_(sql, id, sql_stmt_str(rs, 0));
 	}
 	sql_stmt_destroy(rs);
+
 	return 0;
 }
 

commit 09ea8b7c31f6ff61ccf7bc9c5ca3d37d4d95af1c

Fixed an issue introduced with https://github.com/bbcarchdev/spindle/commit/7f845ba6ca1fada3a034f6473c0c86c140228f62

twine/generate/index-membership.c
@@ -243,19 +243,9 @@ spindle_index_membership_add_uri_(SPINDLEENTRY *data, SQL *sql, const char *id, 
 			spindle_trigger_add(data, uristr, TK_MEMBERSHIP, NULL);
 			return 0;
 		}
-
-		// Add the membership relation
+		spindle_trigger_add(data, uristr, TK_MEMBERSHIP, collid);
 		r = spindle_index_membership_add_(sql, id, collid);
-
-		// If the link was freshly added (r=0) add a matching trigger
-		// r=1 if the link was already there or would have created a loop
-		// r=-1 in case of error trying to add the link
-		if (!r)
-		{
-			spindle_trigger_add(data, uristr, TK_MEMBERSHIP, collid);
-		}
 		free(collid);
-
 		return r;
 	}
 	localuri = spindle_proxy_locate(data->spindle, uristr);
@@ -294,12 +284,11 @@ spindle_index_membership_add_(SQL *sql, const char *id, const char *collid)
 	if(!sql_stmt_eof(rs))
 	{
 		sql_stmt_destroy(rs);
-		return 1;
+		return 0;
 	}
 	sql_stmt_destroy(rs);
 
-	// Ensure we won't create a loop by adding it. If the loop was to be
-	// created don't add the link and return a 1
+	 // Ensure we won't create a loop by adding it.
 	rs = sql_queryf(sql, "SELECT \"id\" FROM \"membership\" WHERE \"id\" = %Q AND \"collection\" = %Q", collid, id);
 	if(!rs)
 	{
@@ -308,7 +297,7 @@ spindle_index_membership_add_(SQL *sql, const char *id, const char *collid)
 	if(!sql_stmt_eof(rs))
 	{
 		sql_stmt_destroy(rs);
-		return 1;
+		return 0;
 	}
 	sql_stmt_destroy(rs);
 
@@ -329,7 +318,5 @@ spindle_index_membership_add_(SQL *sql, const char *id, const char *collid)
 		spindle_index_membership_add_(sql, id, sql_stmt_str(rs, 0));
 	}
 	sql_stmt_destroy(rs);
-
 	return 0;
 }
-