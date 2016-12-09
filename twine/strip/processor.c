2734be2d252014eacaa24c8706467599398dadd6
Christophe Gueret <christophe.gueret@bbc.co.uk>
2016-12-09 14:34:20 +0000

    Only keep triples with a predicate and language tag we will use later

--- twine/strip/processor.c
+++ twine/strip/processor.c
@@ -31,25 +31,31 @@ spindle_strip(twine_graph *graph, void *data)
 {
 	librdf_stream *st;
 	librdf_statement *statement;
-	librdf_node *predicate;
+	librdf_node *predicate, *object;
 	librdf_uri *pred;
-	const char *preduri;
-	int match, r;
+	const char *preduri, *lang;
+	int match, r, literal_ok;
 	size_t c;
 	SPINDLERULES *rules;
+	librdf_model *filtered_model;
 
 	rules = (SPINDLERULES *) data;
+
+	filtered_model = twine_rdf_model_create();
 
 	st = librdf_model_as_stream(graph->store);
 	while(!librdf_stream_end(st))
 	{
-		match = 0;
+
 		statement = librdf_stream_get_object(st);
 		predicate = librdf_statement_get_predicate(statement);
 		if(librdf_node_is_resource(predicate) &&
 		   (pred = librdf_node_get_uri(predicate)) &&
 		   (preduri = (const char *) librdf_uri_as_string(pred)))
 		{
+			// Check all the predicates to see if that one is one we want
+			// to keep
+			match = 0;
 			for(c = 0; c < rules->cpcount; c++)
 			{
 				r = strcmp(rules->cachepreds[c], preduri);
@@ -64,14 +70,35 @@ spindle_strip(twine_graph *graph, void *data)
 					break;
 				}
 			}
-			if(!match)
+
+			// If the object is a literal we need to ensure it is using either
+			// no language or one that we use for the indexing
+			literal_ok = 1;
+			object = librdf_statement_get_object(statement);
+			if(librdf_node_is_literal(object) &&
+			  (lang = librdf_node_get_literal_value_language(object)))
 			{
-				twine_logf(LOG_DEBUG, PLUGIN_NAME ": removing triple with predicate <%s>\n", preduri);
-				librdf_model_remove_statement(graph->store, statement);
+				literal_ok = !strncmp(lang, "en", 2) ||
+						     !strncmp(lang, "cy", 2) ||
+							 !strncmp(lang, "ga", 2) ||
+							 !strncmp(lang, "gd", 2);
+			}
+
+			// If the predicate is acceptable and the eventual literal is too
+			// we add that statement to the filtered model
+			if(match && literal_ok)
+			{
+				twine_logf(LOG_DEBUG, PLUGIN_NAME ": keeping a triple with predicate <%s>\n", preduri);
+				librdf_model_add_statement(filtered_model, statement);
 			}
 		}
 		librdf_stream_next(st);
 	}
 	librdf_free_stream(st);
+
+	// Swap the non filtered and filtered models
+	twine_rdf_model_destroy(graph->store);
+	graph->store = filtered_model;
+
 	return 0;
 }
