46fe6b7ad31b70b959582c52d76df45201f32aa6
Christophe Gueret <christophe.gueret@bbc.co.uk>
2016-12-09 20:09:08 +0000

    Removed legacy code for using SPARQL as backup

--- twine/generate/source.c
+++ twine/generate/source.c
@@ -23,9 +23,7 @@
 
 #include "p_spindle-generate.h"
 
-static int spindle_source_fetch_db_(SPINDLEENTRY *data);
 static int spindle_source_fetch_sparql_(SPINDLEENTRY *data);
-static int spindle_source_sameas_(SPINDLEENTRY *data);
 static int spindle_source_clean_(SPINDLEENTRY *data);
 static int spindle_source_refs_(SPINDLEENTRY *data);
 static int spindle_source_graphs_(SPINDLEENTRY *data);
@@ -60,23 +58,13 @@ spindle_source_fetch_entry(SPINDLEENTRY *data)
 			return 0;
 		}
 	}
-	if(data->db)
+	if(spindle_source_refs_(data))
 	{
-		if(spindle_source_refs_(data))
-		{
-			return -1;
-		}
-		if(spindle_source_fetch_db_(data))
-		{
-			return -1;
-		}
+		return -1;
 	}
-	else
+	if(spindle_source_fetch_sparql_(data))
 	{
-		if(spindle_source_fetch_sparql_(data))
-		{
-			return -1;
-		}
+		return -1;
 	}
 	if(spindle_source_clean_(data))
 	{
@@ -161,7 +149,7 @@ spindle_source_graphs_(SPINDLEENTRY *data)
  * proxy using the database
  */
 static int
-spindle_source_fetch_db_(SPINDLEENTRY *data)
+spindle_source_fetch_sparql_(SPINDLEENTRY *data)
 {
 	int r;
 	size_t c;
@@ -192,97 +180,7 @@ spindle_source_fetch_db_(SPINDLEENTRY *data)
 	return r;
 }
 
-/* Fetch all of the source data about the entities that relate to a particular
- * proxy using SPARQL
- */
-static int
-spindle_source_fetch_sparql_(SPINDLEENTRY *data)
-{
-	/* Find all of the triples related to all of the subjects linked to the
-	 * proxy.
-	 *
-	 * Note that this includes data in both the root and proxy graphs,
-	 * but they will be removed by spindle_cache_source_clean_().
-	 */
-	if(sparql_queryf_model(data->sparql, data->sourcedata,
-						   "SELECT DISTINCT ?s ?p ?o ?g\n"
-						   " WHERE {\n"
-						   "  GRAPH %V {\n"
-						   "   ?s %V %V .\n"
-						   "  }\n"
-						   "  GRAPH ?g {\n"
-						   "   ?s ?p ?o .\n"
-						   "  }\n"
-						   "}",
-						   data->spindle->rootgraph, data->sameas, data->self))
-	{
-		return -1;
-	}
-	if(spindle_source_sameas_(data))
-	{
-		return -1;
-	}
-	return 0;
-}
 
-/* Copy any owl:sameAs references into the proxy graph from the root
- * graph.
- */
-static int
-spindle_source_sameas_(SPINDLEENTRY *data)
-{
-	librdf_statement *query, *st;
-	librdf_node *node;
-	librdf_stream *stream;
-	
-	twine_logf(LOG_DEBUG, PLUGIN_NAME ": **** caching sameAs triples ****\n");
-	query = twine_rdf_st_create();
-	if(!query)
-	{
-		return -1;
-	}
-	node = twine_rdf_node_clone(data->sameas);
-	if(!node)
-	{
-		twine_rdf_st_destroy(query);
-		return -1;
-	}
-	librdf_statement_set_predicate(query, node);
-	node = twine_rdf_node_clone(data->self);
-	if(!node)
-	{
-		twine_rdf_st_destroy(query);
-		return -1;
-	}
-	librdf_statement_set_object(query, node);
-	/* Create a stream querying for (?s owl:sameAs <self>) in the root graph
-	 * from the source data
-	 */
-	stream = librdf_model_find_statements_with_options(data->sourcedata, query, data->spindle->rootgraph, NULL);
-	if(!stream)
-	{
-		twine_logf(LOG_ERR, PLUGIN_NAME ": failed to query model\n");
-		twine_rdf_st_destroy(query);
-		return -1;
-	}
-	while(!librdf_stream_end(stream))
-	{
-		st = librdf_stream_get_object(stream);
-		/* Add the statement to the proxy graph */
-		twine_logf(LOG_DEBUG, PLUGIN_NAME ": *** adding sameAs statement to proxy graph\n");
-		if(twine_rdf_model_add_st(data->proxydata, st, data->graph))
-		{
-			twine_logf(LOG_ERR, PLUGIN_NAME ": failed to add statement to proxy model\n");
-			librdf_free_stream(stream);
-			twine_rdf_st_destroy(query);
-			return -1;
-		}
-		librdf_stream_next(stream);
-	}
-	librdf_free_stream(stream);
-	twine_rdf_st_destroy(query);	
-	return 0;
-}
 
 /* Remove any proxy data from the source model */
 static int
