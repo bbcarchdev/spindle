6fac6ceb58c2661cec1da04e6cb70c7d2f66dbca
Mo McRoberts <mo.mcroberts@bbc.co.uk>
2017-02-10 14:28:04 +0000

    Refactor spindle-strip processor, moving actual decision logic into a list of functions to be called in turn until one indicates that the triple should be retained (related to #101). This commit reverts 2734be2d252014eacaa24c8706467599398dadd6, which should at best be a configuration option.

--- twine/strip/processor.c
+++ twine/strip/processor.c
@@ -2,7 +2,7 @@
  *
  * Author: Mo McRoberts <mo.mcroberts@bbc.co.uk>
  *
- * Copyright (c) 2014-2015 BBC
+ * Copyright (c) 2014-2017 BBC
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -23,6 +23,15 @@
 
 #include "p_spindle-strip.h"
 
+typedef int (*SPINDLESTRIPFN)(SPINDLERULES *restrict rules, librdf_statement *restrict statement, const char *restrict uristr);
+
+static int spindle_strip_is_cachepred_(SPINDLERULES *restrict rules, librdf_statement *restrict st, const char *restrict uristr);
+
+static SPINDLESTRIPFN spindle_strip_rules_[] = {
+	spindle_strip_is_cachepred_,
+	NULL
+};
+
 /* Process a graph, stripping out triples using predicates which don't appear
  * in the rule-base
  */
@@ -31,10 +40,10 @@ spindle_strip(twine_graph *graph, void *data)
 {
 	librdf_stream *st;
 	librdf_statement *statement;
-	librdf_node *predicate, *object;
-	librdf_uri *pred;
-	const char *preduri, *lang;
-	int match, r, literal_ok;
+	librdf_node *predicate;
+	librdf_uri *uri;
+	const char *uristr;
+	int r, n, keep;
 	size_t c;
 	SPINDLERULES *rules;
 	librdf_model *filtered_model;
@@ -43,81 +52,127 @@ spindle_strip(twine_graph *graph, void *data)
 
 	filtered_model = twine_rdf_model_create();
 
-	st = librdf_model_as_stream(graph->store);
-	while(!librdf_stream_end(st))
+	r = 0;
+	for(st = librdf_model_as_stream(graph->store); !librdf_stream_end(st); librdf_stream_next(st))
 	{
-		// Get the current statement from the stream
 		statement = librdf_stream_get_object(st);
-
-		// Skip all statements that do not use a URI for predicate
+		if(!statement)
+		{
+			twine_logf(LOG_ERR, PLUGIN_NAME ": failed to obtain statement from model stream\n");
+			r = -1;
+			break;
+		}
+		
 		predicate = librdf_statement_get_predicate(statement);
+		if(!predicate)
+		{
+			twine_logf(LOG_ERR, PLUGIN_NAME ": failed to obtain predicate from statement\n");
+			r = -1;
+			break;
+		}
 		if(!librdf_node_is_resource(predicate))
+		{
+			twine_logf(LOG_DEBUG, PLUGIN_NAME ": ignoring statement with non-resource predicate\n");
 			continue;
-
-		// Get the char* version of the URI, if that fails exit with an error
-		pred = librdf_node_get_uri(predicate);
-		if (!pred)
-		{
-			librdf_free_stream(st);
-			return -1;
 		}
-		preduri = (const char *) librdf_uri_as_string(pred);
-		if (!preduri)
+		uri = librdf_node_get_uri(predicate);
+		if(!uri)
 		{
-			librdf_free_stream(st);
-			return -1;
+			twine_logf(LOG_ERR, PLUGIN_NAME ": failed to obtain URI from predicate\n");
+			r = -1;
+			break;
+		}
+		uristr = (const char *) librdf_uri_as_string(uri);
+		if(!uristr)
+		{
+			twine_logf(LOG_ERR, PLUGIN_NAME ": failed to obtain string for URI\n");
+			r = -1;
+			break;
 		}
 
-		// Check all the predicates in the rulebase to see if that one is one
-		// we want to keep
-		match = 0;
-		for(c = 0; c < rules->cpcount; c++)
+		/* Loop each of the callback functions in spindle_strip_rules_[] in
+		 * turn, passing the rulebase, the librdf statement, and the
+		 * predicate URI string.
+		 *
+		 * If the callback function returns 1, we mark the triple as being
+		 * left un-stripped and stop processing.
+		 *
+		 * If the callback function returns -1, we abort with an error.
+		 *
+		 * If all of the functions return 0, the triple is stripped
+		 * from the graph.		
+		 */
+		keep = 0;		
+		for(c = 0; spindle_strip_rules_[c]; c++)
 		{
-			r = strcmp(rules->cachepreds[c], preduri);
-			if(!r)
+			n = spindle_strip_rules_[c](rules, statement, uristr);
+			if(n < 0)
 			{
-				match = 1;
+				r = -1;
 				break;
 			}
-			if(r > 0)
+			if(n > 0)
 			{
-				/* The cachepreds list is lexigraphically sorted */
+				keep = 1;
 				break;
 			}
 		}
-
-		// If the object is a literal we need to ensure it is using either
-		// no language or one that we use for the indexing
-		literal_ok = 1;
-		object = librdf_statement_get_object(statement);
-		if(librdf_node_is_literal(object))
+		/* If an error occurred in the loop above, break out of this
+		 * one
+		 */
+		if(r < 0)
 		{
-			lang = librdf_node_get_literal_value_language(object);
-			if (lang)
-			{
-				literal_ok = !strncmp(lang, "en", 2) ||
-							 !strncmp(lang, "cy", 2) ||
-							 !strncmp(lang, "ga", 2) ||
-							 !strncmp(lang, "gd", 2);
-			}
+			break;
 		}
 
-		// If the predicate is acceptable and the eventual literal is too
-		// we add that statement to the filtered model
-		if(match && literal_ok)
+		/* If keep is nonzero, add the triple to the filtered model that will
+		 * replace the input model
+		 */
+		if(keep)
 		{
-			twine_logf(LOG_DEBUG, PLUGIN_NAME ": keeping a triple with predicate <%s>\n", preduri);
+			twine_logf(LOG_DEBUG, PLUGIN_NAME ": keeping a triple with predicate <%s>\n", uristr);
 			librdf_model_add_statement(filtered_model, statement);
 		}
-
-		// Move to the next statement
-		librdf_stream_next(st);
+		else
+		{
+			twine_logf(LOG_DEBUG, PLUGIN_NAME ": stripping a triple with predicate <%s>\n", uristr);
+		}
 	}
 	librdf_free_stream(st);
+	if(!r)
+	{
+		/* If no error occurred during the loop above, replace graph->store
+		 * with our new filtered model.
+		 */
+		twine_rdf_model_destroy(graph->store);
+		graph->store = filtered_model;
+	}
+	return r;
+}
 
-	// Swap the non filtered and filtered models
-	twine_rdf_model_destroy(graph->store);
-	graph->store = filtered_model;
+/* Determine whether a triple should be kept because its predicate is
+ * in the 'cachepred' list from the rulebase
+ */
+static int
+spindle_strip_is_cachepred_(SPINDLERULES *rules, librdf_statement *st, const char *uristr)
+{
+	size_t c;
+	int r;
 
+	(void) st;
+
+	for(c = 0; c < rules->cpcount; c++)
+	{
+		r = strcmp(rules->cachepreds[c], uristr);
+		if(!r)
+		{
+			return 1;
+		}
+		if(r > 0)
+		{
+			/* The cachepreds list is lexigraphically sorted */
+			break;
+		}
+	}
 	return 0;
 }
