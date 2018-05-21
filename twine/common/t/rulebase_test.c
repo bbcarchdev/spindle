874199051cace42a4ca6a6a2ec10632966092dbc
Nicholas Shanks <nicholas.shanks@bbc.co.uk>
2018-05-21 14:08:10 +0100

    Ensure rulebase_add_statement_ returns the result of class_add_matchnode if predicate is spindle:expressedAs and object is a resource.

--- twine/common/t/rulebase_test.c
+++ twine/common/t/rulebase_test.c
@@ -464,6 +464,32 @@ Ensure(spindle_common_rulebase, add_statement_returns_no_error_if_predicate_is_s
 	assert_that(r, is_equal_to(0));
 }
 
+Ensure(spindle_common_rulebase, add_statement_returns_the_result_of_class_add_matchnode_if_predicate_is_spindle_expressedas_and_object_is_a_resource) {
+	librdf_node *subject = (librdf_node *) 0xA01;
+	librdf_node *predicate = (librdf_node *) 0xA02;
+	librdf_node *object = (librdf_node *) 0xA03;
+	librdf_uri *subject_uri = (librdf_uri *) 0xA04;
+	librdf_uri *predicate_uri = (librdf_uri *) 0xA05;
+	char *subject_str = "subject";
+	char *predicate_str = NS_SPINDLE "expressedAs";
+	int result_of_class_add_matchnode = 999;
+
+	expect(librdf_statement_get_subject, will_return(subject));
+	expect(librdf_statement_get_predicate, will_return(predicate));
+	expect(librdf_statement_get_object, will_return(object));
+	expect(librdf_node_is_resource, will_return(1), when(node, is_equal_to(subject)));
+	expect(librdf_node_is_resource, will_return(1), when(node, is_equal_to(predicate)));
+	expect(librdf_node_get_uri, will_return(subject_uri), when(node, is_equal_to(subject)));
+	expect(librdf_uri_as_string, will_return(subject_str), when(uri, is_equal_to(subject_uri)));
+	expect(librdf_node_get_uri, will_return(predicate_uri), when(node, is_equal_to(predicate)));
+	expect(librdf_uri_as_string, will_return(predicate_str), when(uri, is_equal_to(predicate_uri)));
+	expect(librdf_node_is_resource, will_return(1), when(node, is_equal_to(object)));
+	expect(spindle_rulebase_class_add_matchnode, will_return(result_of_class_add_matchnode), when(matchuri, is_equal_to(subject_str)), when(node, is_equal_to(object)));
+
+	int r = spindle_rulebase_add_statement_(NULL, NULL, NULL);
+	assert_that(r, is_equal_to(result_of_class_add_matchnode));
+}
+
 #pragma mark -
 
 TestSuite *create_rulebase_test_suite(void) {
@@ -487,6 +513,7 @@ TestSuite *create_rulebase_test_suite(void) {
 	add_test_with_context(suite, spindle_common_rulebase, add_statement_returns_success_if_predicate_is_rdfs_type_and_object_is_spindle_property);
 	add_test_with_context(suite, spindle_common_rulebase, add_statement_returns_error_if_predicate_is_rdfs_type_and_object_is_spindle_property_but_add_node_fails);
 	add_test_with_context(suite, spindle_common_rulebase, add_statement_returns_no_error_if_predicate_is_spindle_expressedas_but_object_is_not_a_resource);
+	add_test_with_context(suite, spindle_common_rulebase, add_statement_returns_the_result_of_class_add_matchnode_if_predicate_is_spindle_expressedas_and_object_is_a_resource);
 	return suite;
 }
 
