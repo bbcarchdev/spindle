f691c4785d89f2c793c9dd6567ab3347cfec1374
Nicholas Shanks <nicholas.shanks@bbc.co.uk>
2018-05-22 11:13:24 +0100

    Ensure rulebase_cachepred_add adds the uri to the rulebase cachepred list.

---
+++ twine/t/mock_spindle_rulebase.h
@@ -0,0 +1,27 @@
+/**
+ * Copyright 2018 BBC
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+SPINDLERULES *spindle_rulebase_create(const char *path, const struct coref_match_struct *match_types) {
+	return (SPINDLERULES *) mock(path, match_types);
+}
+
+int spindle_rulebase_destroy(SPINDLERULES *rules) {
+	return mock(rules);
+}
+
+int spindle_rulebase_dump(SPINDLERULES *rules) {
+	return mock(rules);
+}
