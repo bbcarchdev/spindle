940598f7d46c5ed62362ec6510cc4ecc3243baaa
Nicholas Shanks <nicholas.shanks@bbc.co.uk>
2018-05-24 10:48:18 +0100

    Cover spindle_rulebase_coref_add_node() and spindle_rulebase_coref_dump().

--- twine/common/t/rulebase_coref_test.c
+++ twine/common/t/rulebase_coref_test.c
@@ -140,6 +140,104 @@ Ensure(spindle_common_rulebase, coref_add_replaces_the_correct_callback_when_mul
 }
 
 #pragma mark -
+#pragma mark spindle_rulebase_coref_add_node
+
+Ensure(spindle_common_rulebase, coref_add_node_marks_the_candidate_predicate_as_being_cacheable_if_there_are_no_match_types) {
+	char *predicate = "predicate";
+	librdf_node *node = NULL;
+	SPINDLERULES rules = { 0 };
+
+	expect(spindle_rulebase_cachepred_add, when(rules, is_equal_to(&rules)), when(uri, is_equal_to(predicate)));
+
+	int r = spindle_rulebase_coref_add_node(&rules, predicate, node);
+	assert_that(r, is_equal_to(0));
+}
+
+Ensure(spindle_common_rulebase, coref_add_node_returns_no_error_and_does_not_mark_the_candidate_predicate_as_being_cacheable_if_the_node_is_not_a_resource) {
+	char *predicate = "predicate";
+	librdf_node *node = (librdf_node *) 0xA01;
+	struct coref_match_struct match_types[] = {
+		{ 0 }
+	};
+	SPINDLERULES rules = { .match_types = match_types };
+
+	expect(librdf_node_is_resource, will_return(0), when(node, is_equal_to(node)));
+
+	int r = spindle_rulebase_coref_add_node(&rules, predicate, node);
+	assert_that(r, is_equal_to(0));
+}
+
+Ensure(spindle_common_rulebase, coref_add_node_returns_no_error_and_does_not_mark_the_candidate_predicate_as_being_cacheable_if_the_match_type_is_unknown) {
+	char *predicate = "predicate";
+	struct coref_match_struct match_types[] = {
+		{ 0 }
+	};
+	librdf_node *node = (librdf_node *) 0xA01;
+	librdf_uri *uri = (librdf_uri *) 0xB01;
+	SPINDLERULES rules = { .match_types = match_types };
+
+	expect(librdf_node_is_resource, will_return(1), when(node, is_equal_to(node)));
+	expect(librdf_node_get_uri, will_return(uri), when(node, is_equal_to(node)));
+	expect(librdf_uri_as_string, will_return(predicate), when(uri, is_equal_to(uri)));
+
+	int r = spindle_rulebase_coref_add_node(&rules, predicate, node);
+	assert_that(r, is_equal_to(0));
+}
+
+Ensure(spindle_common_rulebase, coref_add_node_marks_the_candidate_predicate_as_being_cacheable_if_the_match_type_is_known) {
+	char *predicate_1 = "predicate 1";
+	char *predicate_2 = "predicate 2";
+	int (*callback_1)() = (int (*)()) 0xA01;
+	int (*callback_2)() = (int (*)()) 0xA02;
+	struct coref_match_struct match_types[] = {
+		{ .predicate = predicate_1, .callback = callback_1 },
+		{ .predicate = predicate_2, .callback = callback_2 },
+		{ 0 }
+	};
+	librdf_node *node = (librdf_node *) 0xB01;
+	librdf_uri *uri = (librdf_uri *) 0xB02;
+	SPINDLERULES rules = { .match_types = match_types };
+
+	expect(librdf_node_is_resource, will_return(1), when(node, is_equal_to(node)));
+	expect(librdf_node_get_uri, will_return(uri), when(node, is_equal_to(node)));
+	expect(librdf_uri_as_string, will_return(predicate_2), when(uri, is_equal_to(uri)));
+	expect(spindle_rulebase_cachepred_add, when(rules, is_equal_to(&rules)), when(uri, is_equal_to(predicate_2)));
+
+	int r = spindle_rulebase_coref_add_node(&rules, predicate_2, node);
+	assert_that(r, is_equal_to(1));
+	assert_that(rules.corefcount, is_equal_to(1));
+	assert_that(rules.corefsize, is_greater_than(0));
+	assert_that(rules.coref, is_non_null);
+	assert_that(rules.coref[0].predicate, is_equal_to_string(predicate_2));
+	assert_that(rules.coref[0].callback, is_equal_to(callback_2));
+}
+
+#pragma mark -
+#pragma mark spindle_rulebase_coref_add_node
+
+Ensure(spindle_common_rulebase, coref_dump_only_has_side_effects) {
+	char *predicate_1 = "predicate 1";
+	char *predicate_2 = "predicate 2";
+	struct coref_match_struct match_types[] = {
+		{ .predicate = predicate_1 },
+		{ .predicate = predicate_2 },
+		{ 0 }
+	};
+	struct coref_match_struct corefs[] = {
+		{ .predicate = predicate_1 },
+		{ .predicate = predicate_2 }
+	};
+	SPINDLERULES rules = {
+		.match_types = match_types,
+		.coref = corefs,
+		.corefcount = sizeof corefs / sizeof corefs[0]
+	};
+
+	int r = spindle_rulebase_coref_dump(&rules);
+	assert_that(r, is_equal_to(0));
+}
+
+#pragma mark -
 
 TestSuite *create_rulebase_coref_test_suite(void) {
 	TestSuite *suite = create_test_suite();
