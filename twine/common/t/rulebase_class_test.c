e5b9850426923e8a740cf5ba8a9144f3924baac6
Nicholas Shanks <nicholas.shanks@bbc.co.uk>
2018-05-30 17:41:22 +0100

    Stop learning from rulebase_class_test.c

--- twine/common/t/rulebase_class_test.c
+++ twine/common/t/rulebase_class_test.c
@@ -46,7 +46,7 @@ typedef enum {
 #include "../rulebase-class.c"
 
 Describe(spindle_common_rulebase);
-BeforeEach(spindle_common_rulebase) { cgreen_mocks_are(learning_mocks); always_expect(twine_logf); die_in(1); }
+BeforeEach(spindle_common_rulebase) { always_expect(twine_logf); die_in(1); }
 AfterEach(spindle_common_rulebase) {}
 
 /*
